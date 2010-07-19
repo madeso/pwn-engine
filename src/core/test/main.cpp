fb26567bf4f863e914cae85e576362410f049fc3
Gustav <sir.gustav.the.coder@gmail.com>
2010-07-19 18:34:43 +0200

    forgot to commit removed files

--- src/core/test/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-#include <pwn/assert.h>
-
-int main(int argc, char **argv)
-{
-	pwn::assert::Init(pwn::assert::AlwaysThrow);
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}

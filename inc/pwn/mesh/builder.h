8ec1f7ef322ae7cadee1f5035ef0b958f57451fd
Gustav <sir.gustav.the.coder@gmail.com>
2010-09-01 06:42:01 +0200

    brought back old mesh functions to the builder

--- inc/pwn/mesh/builder.h
+++ inc/pwn/mesh/builder.h
@@ -6,6 +6,7 @@
 
 #include <boost/shared_ptr.hpp>
 #include <vector>
+#include <map>
 #include <pwn/string.h>
 #include <pwn/mesh/mesh.h>
 
@@ -22,16 +23,25 @@ namespace pwn
 
 		class Builder
 		{
+		public:
 			void clear();
 			Triangle::index addTextCoord(const math::vec2& v);
-			Triangle::index addPosition(const math::vec3& v, Triangle::index bone);
+			Triangle::index addPosition(const Point& pos);
+			Triangle::index addPosition(const math::vec3& pos, BoneIndex bone); // syntax sugar
+			Triangle::index addNormal(const math::vec3& norm);
 			void addTriangle(pwn::uint32 material, const Triangle& t);
+			Triangle::index addMaterial(Mesh::MaterialPtr m);
+
 			void addQuad(bool reverse, pwn::uint32 material, const Triangle::Vertex& v0, const Triangle::Vertex& v1, const Triangle::Vertex& v2, const Triangle::Vertex& v3);
 			void addFace(pwn::uint32 material, const std::vector<Triangle::Vertex>& vertices);
 			void setBox(boost::shared_ptr<Material> material, real w, real h, real d, bool faceOut);
 			void buildNormals();
-			
-			std::vector<Triangle> triangles;
+
+			void makeMesh(Mesh& mesh) const;
+
+			typedef std::vector<Triangle> TriList;
+			typedef std::map<pwn::uint32, TriList> TriMap;
+			TriMap triangles;
 			std::vector<Point> positions;
 			std::vector<math::vec3> normals;
 			std::vector<math::vec2> texcoords;
--- src/mesh/builder.cpp
+++ src/mesh/builder.cpp
@@ -31,6 +31,55 @@ namespace pwn
 			return mesh;
 		}
 
+		Mesh* InvertNormals(Mesh* mesh)
+		{
+			BOOST_FOREACH(math::vec3& n, mesh->normals)
+			{
+				n = -n;
+			}
+
+			return mesh;
+		}
+
+		namespace
+		{
+			void KeepLast(pwn::string& t, const char c)
+			{
+				std::size_t i = t.find_last_of(c);
+				if( i != pwn::string::npos )
+				{
+					t = t.substr(i+1);
+				}
+			}
+
+			pwn::string MoveTexture(const pwn::string& texture, const pwn::string& newFolder)
+			{
+				if( pwn::core::Trim(texture) == "" ) return "";
+				pwn::string t = texture;
+				KeepLast(t, '\\');
+				KeepLast(t, '/');
+				KeepLast(t, '|');
+				if( pwn::core::EndsWith(t, ".tif") ) t = pwn::core::StringReplace(t, ".tif", ".png");
+				if( newFolder.empty() ) return t;
+				else return newFolder + "/" + t;
+			}
+
+			void MoveTextures(Mesh::MaterialPtr mat, const pwn::string& newFolder)
+			{
+				mat->texture_diffuse = MoveTexture(mat->texture_diffuse, newFolder);
+			}
+		}
+
+		void MoveTextures(Mesh* mesh, const pwn::string& newFolder)
+		{
+			for(std::size_t i = 0; i < mesh->materials.size(); ++i)
+			{
+				MoveTextures(mesh->materials[i], newFolder);
+			}
+		}
+
+		// ==================================================================================================================================
+
 		namespace // local
 		{
 			Triangle::Vertex v(Triangle::index p, Triangle::index t)
@@ -39,6 +88,79 @@ namespace pwn
 			}
 		}
 
+		// ==================================================================================================================================
+
+		void Builder::clear()
+		{
+			triangles.clear();
+			positions.clear();
+			normals.clear();
+			texcoords.clear();
+			bones.clear();
+			materials.clear();
+		}
+
+		Triangle::index Builder::addTextCoord(const math::vec2& tc)
+		{
+			texcoords.push_back(tc);
+			return static_cast<Triangle::index>(texcoords.size()-1);
+		}
+
+		Triangle::index Builder::addPosition(const Point& pos)
+		{
+			positions.push_back(pos);
+			return static_cast<Triangle::index>(positions.size()-1);
+		}
+
+		Triangle::index Builder::addPosition(const math::vec3& pos, BoneIndex bone)
+		{
+			return addPosition(Point(pos, bone));
+		}
+		
+		Triangle::index Builder::addNormal(const math::vec3& norm)
+		{
+			normals.push_back(norm);
+			return static_cast<Triangle::index>(normals.size()-1);
+		}
+
+		void Builder::addTriangle(pwn::uint32 material, const Triangle& t)
+		{
+			triangles[material].push_back(t);
+		}
+
+		Triangle::index Builder::addMaterial(Mesh::MaterialPtr m)
+		{
+			materials.push_back(m);
+			return static_cast<Triangle::index>(materials.size()-1);
+		}
+		
+		void Builder::addQuad(bool reverse, pwn::uint32 material, const Triangle::Vertex& v0, const Triangle::Vertex& v1, const Triangle::Vertex& v2, const Triangle::Vertex& v3)
+		{
+			if( reverse )
+			{
+				addTriangle(material, Triangle(v2, v1, v0) );
+				addTriangle(material, Triangle(v3, v2, v0) );
+			}
+			else
+			{
+				addTriangle(material, Triangle(v0, v1, v2) );
+				addTriangle(material, Triangle(v0, v2, v3) );
+			}
+		}
+
+		void Builder::addFace(pwn::uint32 material, const std::vector<Triangle::Vertex>& vertices)
+		{
+			// we currently doesnt support ton-triangular faces so - triangulate it
+			const std::vector<Triangle::Vertex>::size_type size = vertices.size();
+			bool added = false;
+			for(std::vector<Triangle::Vertex>::size_type i=2; i<size; ++i)
+			{
+				addTriangle(material, Triangle(vertices[0], vertices[i-1], vertices[i]));
+				added = true;
+			}
+			if( false == added ) throw "Unable to triangulate face";
+		}
+		
 		void Builder::setBox(boost::shared_ptr<Material> material, real w, real h, real d, bool faceOut)
 		{
 			using math::vec2;
@@ -73,40 +195,29 @@ namespace pwn
 			materials.push_back(material);
 		}
 
-		void Builder::addQuad(bool reverse, pwn::uint32 material, const Triangle::Vertex& v0, const Triangle::Vertex& v1, const Triangle::Vertex& v2, const Triangle::Vertex& v3)
-		{
-			if( reverse )
-			{
-				addTriangle(material, Triangle(v2, v1, v0) );
-				addTriangle(material, Triangle(v3, v2, v0) );
-			}
-			else
-			{
-				addTriangle(material, Triangle(v0, v1, v2) );
-				addTriangle(material, Triangle(v0, v2, v3) );
-			}
-		}
-
 		void Builder::buildNormals()
 		{
 			using math::vec3;
 			std::vector<vec3> vertexNormalsSum(positions.size(), vec3(0,0,0));
-			BOOST_FOREACH(Triangle& t, triangles)
+			BOOST_FOREACH(TriMap::value_type& tr, triangles)
 			{
-				const vec3 p0 = positions[t[0].location].location;
-				const vec3 p1 = positions[t[1].location].location;
-				const vec3 p2 = positions[t[2].location].location;
+				BOOST_FOREACH(Triangle& t, tr.second)
+				{
+					const vec3 p0 = positions[t[0].location].location;
+					const vec3 p1 = positions[t[1].location].location;
+					const vec3 p2 = positions[t[2].location].location;
 
-				const vec3 d0 = math::FromTo(p1, p0);
-				const vec3 d1 = math::FromTo(p1, p2);
+					const vec3 d0 = math::FromTo(p1, p0);
+					const vec3 d1 = math::FromTo(p1, p2);
 
-				const vec3 faceNormal = math::crossNorm(d0, d1);
+					const vec3 faceNormal = math::crossNorm(d0, d1);
 
-				vertexNormalsSum[t[0].location] += faceNormal;
-				vertexNormalsSum[t[1].location] += faceNormal;
-				vertexNormalsSum[t[2].location] += faceNormal;
+					vertexNormalsSum[t[0].location] += faceNormal;
+					vertexNormalsSum[t[1].location] += faceNormal;
+					vertexNormalsSum[t[2].location] += faceNormal;
 
-				for(int i=0; i<3; ++i) t[i].normal = t[i].location;
+					for(int i=0; i<3; ++i) t[i].normal = t[i].location;
+				}
 			}
 
 			normals.clear();
@@ -116,61 +227,6 @@ namespace pwn
 			}
 		}
 
-		Mesh* InvertNormals(Mesh* mesh)
-		{
-			BOOST_FOREACH(math::vec3& n, mesh->normals)
-			{
-				n = -n;
-			}
-
-			return mesh;
-		}
-
-		void Builder::addFace(pwn::uint32 material, const std::vector<Triangle::Vertex>& vertices)
-		{
-			// we currently doesnt support ton-triangular faces so - triangulate it
-			const std::vector<Triangle::Vertex>::size_type size = vertices.size();
-			bool added = false;
-			for(std::vector<Triangle::Vertex>::size_type i=2; i<size; ++i)
-			{
-				addTriangle(material, Triangle(vertices[0], vertices[i-1], vertices[i]));
-				added = true;
-			}
-			if( false == added ) throw "Unable to triangulate face";
-		}
-
-		void KeepLast(pwn::string& t, const char c)
-		{
-			std::size_t i = t.find_last_of(c);
-			if( i != pwn::string::npos )
-			{
-				t = t.substr(i+1);
-			}
-		}
-
-		pwn::string MoveTexture(const pwn::string& texture, const pwn::string& newFolder)
-		{
-			if( pwn::core::Trim(texture) == "" ) return "";
-			pwn::string t = texture;
-			KeepLast(t, '\\');
-			KeepLast(t, '/');
-			KeepLast(t, '|');
-			if( pwn::core::EndsWith(t, ".tif") ) t = pwn::core::StringReplace(t, ".tif", ".png");
-			if( newFolder.empty() ) return t;
-			else return newFolder + "/" + t;
-		}
-
-		void MoveTextures(Mesh::MaterialPtr mat, const pwn::string& newFolder)
-		{
-			mat->texture_diffuse = MoveTexture(mat->texture_diffuse, newFolder);
-		}
-
-		void MoveTextures(Mesh* mesh, const pwn::string& newFolder)
-		{
-			for(std::size_t i = 0; i < mesh->materials.size(); ++i)
-			{
-				MoveTextures(mesh->materials[i], newFolder);
-			}
-		}
+		void Builder::makeMesh(Mesh& mesh) const;
 	}
 }

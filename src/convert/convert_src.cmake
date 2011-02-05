3f6e8d491e01cb2c256100ea9123a20136a78c64
Gustav <sir.gustav.the.coder@gmail.com>
2011-02-06 00:03:02 +0100

    mnoved convert from src subdir

---
+++ src/convert/convert_src.cmake
@@ -0,0 +1,19 @@
+# atuogenerated file from cmakegen.py through listdir
+# do not edit, since this file might get overwritten
+# delete the comment where this file is included to remove the autogeneration
+
+set ( convert_src
+	3ds.cpp
+	3ds.hpp
+	Converter.cpp
+	Converter.hpp
+	MilkshapeAscii.cpp
+	MilkshapeAscii.hpp
+	MilkshapeBinary.cpp
+	MilkshapeBinary.hpp
+	MilkshapeCommon.cpp
+	MilkshapeCommon.hpp
+	WavefrontObj.cpp
+	WavefrontObj.hpp
+	convert.cpp
+)

07d515b8e71ef7ea964ab154fc836371220c9b6e
Gustav <sir.gustav.the.coder@gmail.com>
2011-02-05 14:09:43 +0100

    fixed autogen inc filenames

--- external/physfspp/physfscpp.cmake
+++ external/physfspp/physfscpp.cmake
@@ -2,7 +2,7 @@
 # do not edit, since this file might get overwritten
 # delete the comment where this file is included to remove the autogeneration
 
-set ( autogen___external_physfspp
+set ( autogen_physfscpp
 	../../external/physfspp/PhysFS++.cpp
 	../../external/physfspp/PhysFS++.h
 	../../external/physfspp/example.cpp

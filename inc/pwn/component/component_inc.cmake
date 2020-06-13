cff6b78c2bed4775339de10690d95a29658feb2b
Gustav <sir.gustav.the.coder@gmail.com>
2020-06-13 15:34:12 +0200

    ran cmakegen

---
+++ inc/pwn/component/component_inc.cmake
@@ -0,0 +1,18 @@
+# atuogenerated file from cmakegen.py through listdir
+# do not edit, since this file might get overwritten
+# delete the comment where this file is included to remove the autogeneration
+
+set ( component_inc
+	../../inc/pwn/component/component.h
+	../../inc/pwn/component/componentdef.h
+	../../inc/pwn/component/componentstd.h
+	../../inc/pwn/component/engine.h
+	../../inc/pwn/component/eventargs.h
+	../../inc/pwn/component/object.h
+	../../inc/pwn/component/objectdef.h
+	../../inc/pwn/component/property.h
+	../../inc/pwn/component/propertydef.h
+	../../inc/pwn/component/propertystd.h
+	../../inc/pwn/component/world.h
+	../../inc/pwn/component/worlddef.h
+)
---
+++ src/component/component_src.cmake
@@ -0,0 +1,18 @@
+# atuogenerated file from cmakegen.py through listdir
+# do not edit, since this file might get overwritten
+# delete the comment where this file is included to remove the autogeneration
+
+set ( component_src
+	component.cpp
+	componentdef.cpp
+	componentstd.cpp
+	engine.cpp
+	eventargs.cpp
+	object.cpp
+	objectdef.cpp
+	property.cpp
+	propertydef.cpp
+	propertystd.cpp
+	world.cpp
+	worlddef.cpp
+)
